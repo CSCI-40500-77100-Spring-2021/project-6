678216795a7d99a5021dd3f26ed205c2473b4815
Deondre De Freitas <deondrede@gmail.com>
2021-03-03 23:00:00 -0500

    Html and other associated files

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('anime_recommender.urls')),
+    path('', include('maven.urls')),
 ]
