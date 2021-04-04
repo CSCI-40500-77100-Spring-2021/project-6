36142c89ad8ded22112b9211822d647d075e611b
Deondre De Freitas <deondrede@gmail.com>
2021-04-04 17:32:55 -0400

    added table for keeping track of books in the store

--- maven/models.py
+++ maven/models.py
@@ -6,6 +6,11 @@ class User(models.Model):
     username = models.CharField(blank=True,max_length=255)
     password = models.CharField(blank=True,max_length=30)
         
+class Books(models.Model):
+    ISBN = models.CharField(blank=True,max_length=255)
+    title = models.CharField(blank=True,max_length=255)
+    author = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=255)
 
 def __str__(self):
     return self.name
