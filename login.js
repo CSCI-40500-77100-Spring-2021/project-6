51130d59b33933ec2833f294166a6cb89d5b83ae
Deondre De Freitas <deondrede@gmail.com>
2021-04-13 21:12:28 -0400

    making sure everything is working

--- login.js
+++ login.js
@@ -3,12 +3,10 @@ function onLogin(){
         for (i in data){
             console.log(data);
             if (data[i].fields.username == document.getElementById("username").value && data[i].fields.password == document.getElementById("password").value){
-                document.cookie = "username=" + document.getElementById("username").value + "; path=/";
+                window.localStorage.setItem("username",username);
                 window.location.replace("browse.html");
                 console.log("success");
-            }
-            else {
-                console.log("fail");
+                break;
             }
         }
     });
