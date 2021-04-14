3fce796763135940a7500e4fd49c383a21937db6
Deondre De Freitas <deondrede@gmail.com>
2021-04-14 14:35:13 -0400

    local storage fix

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function onLogin(){
         for (i in data){
             console.log(data);
             if (data[i].fields.username == document.getElementById("username").value && data[i].fields.password == document.getElementById("password").value){
-                window.localStorage.setItem("username",username);
+                window.localStorage.setItem("username",data[i].fields.username);
                 window.location.replace("browse.html");
                 console.log("success");
                 break;
