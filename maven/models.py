e90109d062ba8270233abb1d387da7b77f4b4ba1
Deondre De Freitas <deondrede@gmail.com>
2021-04-14 14:44:42 -0400

    added book table

--- maven/models.py
+++ maven/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.db.models.fields import EmailField
 
@@ -6,6 +7,12 @@ class User(models.Model):
     username = models.CharField(blank=True,max_length=255)
     password = models.CharField(blank=True,max_length=30)
         
+class Books(models.Model):
+    ISBN = models.CharField(blank=True,max_length=255)
+    title = models.CharField(blank=True,max_length=255)
+    author = models.CharField(blank=True,max_length=255)
+    genre = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=255)
 
 def __str__(self):
     return self.name
