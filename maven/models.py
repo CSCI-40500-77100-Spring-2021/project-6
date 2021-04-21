119e57d1a68461ad86b66462e49f0500178fa1ae
Deondre De Freitas <38331348+Deondrede@users.noreply.github.com>
2021-04-21 19:08:32 -0400

    Update models.py

--- maven/models.py
+++ maven/models.py
@@ -1,18 +1,19 @@
-
 from django.db import models
 from django.db.models.fields import EmailField
 
 # Create your models here.
 class User(models.Model):
+    ID = models.UUIDField(primary_key=True,default=True,editable=False)
     username = models.CharField(blank=True,max_length=255)
     password = models.CharField(blank=True,max_length=30)
         
 class Books(models.Model):
+    owner_ID = models.UUIDField(blank=True,editable=False,default=None)
     ISBN = models.CharField(blank=True,max_length=255)
     title = models.CharField(blank=True,max_length=255)
     author = models.CharField(blank=True,max_length=255)
     genre = models.CharField(blank=True,max_length=255)
-    description = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=5000)
 
 def __str__(self):
     return self.name
